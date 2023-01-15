af773004eb1eac9d46d6fc0c709a235d36df548c
Jan Kousalík <kousaja1@cvut.cz>
2023-01-15 11:31:19 +0100

    Custom theme for form texttype

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -31,6 +31,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Password',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
