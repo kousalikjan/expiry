b1615c5f17327ee2cf3bb617ca2a1696158cc208
Jan Kousalík <kousaja1@cvut.cz>
2023-02-01 13:09:07 +0100

    Import turbo

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
