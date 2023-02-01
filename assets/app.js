4b05c27695536242905426901f0a6cfb11e0181c
Jan Kousalík <kousaja1@cvut.cz>
2023-02-01 14:56:53 +0100

    Sweetalert modal bug

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,22 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import Swal from 'sweetalert2';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+document.addEventListener('turbo:before-cache', () => {
+    if (document.body.classList.contains('modal-open')) {
+        const modalEl = document.querySelector('.modal');
+        const modal = Modal.getInstance(modalEl);
+        modalEl.classList.remove('fade');
+        modal._backdrop._config.isAnimated = false;
+        modal.hide();
+        modal.dispose();
+    }
+    if (Swal.isVisible()) {
+        Swal.getPopup().style.animationDuration = '0ms'
+        Swal.close();
+    }
+});
