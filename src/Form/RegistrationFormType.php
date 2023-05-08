7c0fb1cf272d3e151c77f74bd6d4b66fdbe148e5
Jan Kousalík <kousaja1@cvut.cz>
2023-05-08 10:59:11 +0200

    Links from login to registration and vice versa

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,14 +22,6 @@ class RegistrationFormType extends AbstractType
             ->add('email', EmailType::class, [
             'constraints' => new Regex('/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/')
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
